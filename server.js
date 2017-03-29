17854112880c2d13a1c40dcfa24da240cd880ff2
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-03-29 23:25:56 +0600

    v1

--- server.js
+++ server.js
@@ -1,85 +1,134 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+
 const L = require('lodash');
+const uid = require('uid');
 
 const JsonDB = require('node-json-db');
-const db = new JsonDB("Keywords", true, false);
+const db = new JsonDB('Keywords', true, false);
+
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-function checkToken(req, res, next) {
-    if (req.query.access_token && req.query.access_token === ACCESS_TOKEN ) {
-        next();
-    } else {
-       res.json({status:403, message: 'Access Token Invalid'})
-    }
+function checkToken(request, response, next) {
+  const { access_token } = request.query;
+  
+  access_token && access_token === ACCESS_TOKEN
+    ? next()
+    : response.status(403).json({
+        status:403, 
+        message: 'Access Token Invalid'
+      })
+    ;
 }
 
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use( bodyParser.json() );
 app.use( checkToken );
+// app.use( express.static('public') );
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.get("/", (request, response) => response.json({ 
+  about: 'Search SMS Keywords Storage' 
+}));
 
-// http://expressjs.com/en/starter/basic-routing.html
-// app.get("/", function (request, response) {
-//   response.sendFile(__dirname + '/views/index.html');
-// });
-
-app.get("/", function (request, response) {
-  // response.sendFile(__dirname + '/views/index.html');
-  response.json({ about: 'Search SMS Keywords Storage' });
+app.get("/v1/keywords", (request, response) => {
+  const items = db.getData("/");
+  response.json( items );
 });
 
-app.get("/v1/keywords", function (request, response) {
-  const dicts = db.getData("/");
-  const resp = L.isEmpty( dicts ) ? { items:[] } : { items: L.keys(dicts) };
-  response.json( resp );
+app.get("/v1/keywords/:id", (request, response) => {
+  let dict;
+  try {
+    dict = db.getData("/" + request.params.id);
+  } catch(error) {
+    response.status(404).json({
+      status:404, 
+      message: 'Dictionary not found'
+    })
+  }  
+  response.json( dict );
 });
 
-app.post("/v1/keywords", function (request, response) {
+app.post("/v1/keywords", (request, response) => {
   if ( !request.body.name ) {
-    return response.json({status:400, message: 'Dictionary name required'})
+    return response.status(400).json({
+      status:400, 
+      message: 'Dictionary name required'
+    })
   }
   
-  db.push("/",{ name: request.body.name, items: [] });
+  const { name } = request.body ;
+  const id = uid();
+  
+  db.push('/' + id,{ id, name, items: [] });
+  response.json({ name, id });
+});
+
+app.post("/v1/keywords/:id", (request, response) => {
+  let dict;
+  
+  try {
+    dict = db.getData("/" + request.params.id);
+  } catch(error) {
+    response.status(404).json({
+      status:404, 
+      message: 'Dictionary not found'
+    })
+  } 
+  
+  if ( !request.body.name ) {
+    return response.json({
+      status:400, 
+      message: 'Keyword name required'
+    })
+  }
+  
+  const { name } = request.body ;
+  const id = uid();
+  
+  if( dict.items.indexOf(name) === -1) {
+    dict.items.push(name);
+    db.push('/' + request.params.id, dict);
+  }
   response.sendStatus(200);
 });
 
-// db.delete("/test1");
-
-app.get("/v1/flush", function (request, response) {
+app.delete("/v1/keywords", (request, response) => {
   db.delete("/");
   response.sendStatus(200);
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
+app.delete("/v1/keywords/:id", (request, response) => {
+  let dict;
+  
+  try {
+    dict = db.getData("/" + request.params.id);
+  } catch(error) {
+    response.status(404).json({
+      status:404, 
+      message: 'Dictionary not found'
+    })
+  } 
+  
+  
+  if ( request.query.keyword ) {
+    const index = dict.items.indexOf(request.query.keyword);
+    if ( index !== -1 ) {
+      dict.items.splice(index, 1);
+      db.push('/' + request.params.id, dict);
+      return response.sendStatus(200);
+    } else {
+      return response.status(404).json({
+        status:404, 
+        message: 'Keyword not found'
+      })
+    }    
+  }  
+  
+  db.delete("/" + request.params.id);
   response.sendStatus(200);
 });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+  console.log('App is listening on port ' + listener.address().port);
 });
