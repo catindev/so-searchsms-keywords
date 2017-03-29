471b89670b65c364b9ac56d77b036b10471ca058
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-03-29 23:47:00 +0600

    default token

--- server.js
+++ server.js
@@ -8,85 +8,85 @@ const uid = require('uid');
 const JsonDB = require('node-json-db');
 const db = new JsonDB('Keywords', true, false);
 
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN || '6zGxe6S7rI';
 
 function checkToken(request, response, next) {
   const { access_token } = request.query;
-  
+
   access_token && access_token === ACCESS_TOKEN
     ? next()
     : response.status(403).json({
-        status:403, 
-        message: 'Access Token Invalid'
-      })
+      status: 403,
+      message: 'Access Token Invalid'
+    })
     ;
 }
 
-app.use( bodyParser.urlencoded({ extended: true }) );
-app.use( bodyParser.json() );
-app.use( checkToken );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(checkToken);
 // app.use( express.static('public') );
 
-app.get("/", (request, response) => response.json({ 
-  about: 'Search SMS Keywords Storage' 
+app.get("/", (request, response) => response.json({
+  about: 'Search SMS Keywords Storage'
 }));
 
 app.get("/v1/keywords", (request, response) => {
   const items = db.getData("/");
-  response.json( items );
+  response.json(items);
 });
 
 app.get("/v1/keywords/:id", (request, response) => {
   let dict;
   try {
     dict = db.getData("/" + request.params.id);
-  } catch(error) {
+  } catch (error) {
     response.status(404).json({
-      status:404, 
+      status: 404,
       message: 'Dictionary not found'
     })
-  }  
-  response.json( dict );
+  }
+  response.json(dict);
 });
 
 app.post("/v1/keywords", (request, response) => {
-  if ( !request.body.name ) {
+  if (!request.body.name) {
     return response.status(400).json({
-      status:400, 
+      status: 400,
       message: 'Dictionary name required'
     })
   }
-  
-  const { name } = request.body ;
+
+  const { name } = request.body;
   const id = uid();
-  
-  db.push('/' + id,{ id, name, items: [] });
+
+  db.push('/' + id, { id, name, items: [] });
   response.json({ name, id });
 });
 
 app.post("/v1/keywords/:id", (request, response) => {
   let dict;
-  
+
   try {
     dict = db.getData("/" + request.params.id);
-  } catch(error) {
+  } catch (error) {
     response.status(404).json({
-      status:404, 
+      status: 404,
       message: 'Dictionary not found'
     })
-  } 
-  
-  if ( !request.body.name ) {
+  }
+
+  if (!request.body.name) {
     return response.json({
-      status:400, 
+      status: 400,
       message: 'Keyword name required'
     })
   }
-  
-  const { name } = request.body ;
+
+  const { name } = request.body;
   const id = uid();
-  
-  if( dict.items.indexOf(name) === -1) {
+
+  if (dict.items.indexOf(name) === -1) {
     dict.items.push(name);
     db.push('/' + request.params.id, dict);
   }
@@ -100,31 +100,31 @@ app.delete("/v1/keywords", (request, response) => {
 
 app.delete("/v1/keywords/:id", (request, response) => {
   let dict;
-  
+
   try {
     dict = db.getData("/" + request.params.id);
-  } catch(error) {
+  } catch (error) {
     response.status(404).json({
-      status:404, 
+      status: 404,
       message: 'Dictionary not found'
     })
-  } 
-  
-  
-  if ( request.query.keyword ) {
+  }
+
+
+  if (request.query.keyword) {
     const index = dict.items.indexOf(request.query.keyword);
-    if ( index !== -1 ) {
+    if (index !== -1) {
       dict.items.splice(index, 1);
       db.push('/' + request.params.id, dict);
       return response.sendStatus(200);
     } else {
       return response.status(404).json({
-        status:404, 
+        status: 404,
         message: 'Keyword not found'
       })
-    }    
-  }  
-  
+    }
+  }
+
   db.delete("/" + request.params.id);
   response.sendStatus(200);
 });
